ca6137d9a4e571f458fab3e77a69b67aa85cc42e
Nahush22 <nahushrockz22@gmail.com>
2020-02-25 18:33:41 +0530

    first commit

---
+++ app/src/main/java/com/example/recyclerexample2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.recyclerexample2;
+
+import androidx.appcompat.app.AlertController;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private int[]
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+    }
+}
